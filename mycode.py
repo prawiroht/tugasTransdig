df1eedd1ba37da9eb10a7ea8b5a65fdfc939d244
prawiroht <prawiroht@gmail.com>
2019-12-22 16:05:03 +0700

    update lagi

--- mycode.py
+++ mycode.py
@@ -1,3 +1,5 @@
+# IMPORT LIBRARY
+# 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -8,6 +10,8 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Dense, Conv2D, MaxPooling2D, Flatten, Dropout
 
+# IMPORT INPUT (CITRA)
+#
 img_width, img_height = 64, 64
 # data path
 train_data_dir = 'dataset/train'
@@ -27,7 +31,8 @@ validation_generator = datagen.flow_from_directory(
         target_size=(img_width, img_height),
         class_mode='binary')
 
-# create machine learning model
+# BUAT MESIN/MODEL CNN
+# 
 model = Sequential()
 model.add(Conv2D(32, kernel_size=(5, 5), activation = 'relu', input_shape=(img_width, img_height, 3) ))
 model.add(MaxPooling2D(pool_size = (2, 2)))
@@ -39,29 +44,23 @@ model.add(Dense(1, activation = 'sigmoid'))
 model.compile(loss = keras.losses.binary_crossentropy,
               optimizer=keras.optimizers.Adam(lr=0.00005, beta_2=0.9),
               metrics=['accuracy'])
+
+# TRAINING & VALIDATION MODEL CNN
+#       
 model_hist = model.fit_generator(train_generator, 
                                  epochs=20,
                                  validation_data=validation_generator)
 
-# Save the weights
-model.save_weights('model_weights.h5')
 
-# Save the model architecture
+# SAVE MODEL, MODEL ARCHITECTURE & WEIGHTS
+# 
+model.save('modelx.h5')
 with open('model_architecture.json', 'w') as f:
     f.write(model.to_json())
+model.save_weights('model_weights.h5')
 
-model.save('modelx.h5')
-
-plt.figure(figsize=(10, 6))
-plt.plot(model_hist.history['loss'], label='loss')
-plt.plot(model_hist.history['val_loss'], label='val_loss')
-plt.title('Grafik Loss')
-plt.xlabel('Epoch')
-plt.ylabel('Loss')
-plt.grid()
-plt.legend()
-plt.show()
-
+# GAMBAR GRAFIK/PLOT AKURASI KLASIFIKASI OLEH MODEL CNN
+# 
 plt.figure(figsize=(10, 6))
 plt.plot(model_hist.history['val_accuracy'], label='val_accuracy')
 plt.plot(model_hist.history['accuracy'], label='accuracy')
